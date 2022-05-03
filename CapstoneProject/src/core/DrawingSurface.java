37e4cd79ca9791f568661fa611e9156ac4c9075a
nishoof <nanand755@student.fuhsd.org>
2022-05-03 10:25:19 -0700

    ScreenGame made

--- CapstoneProject/src/core/DrawingSurface.java
+++ CapstoneProject/src/core/DrawingSurface.java
@@ -21,6 +21,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import databaseData.Post;
 import databaseData.UserPost;
 import gameElements.screens.Screen;
+import gameElements.screens.ScreenGame;
 import gameElements.screens.ScreenMenu;
 import gameElements.screens.ScreenNameCreate;
 import gameElements.screens.ScreenQueue;
@@ -98,7 +99,11 @@ public class DrawingSurface extends PApplet {
 		ScreenQueue screen4 = new ScreenQueue(this, ref);
 		screens.add(screen4);
 		
-		activeScreen = screens.get(0);
+		ScreenGame screen5 = new ScreenGame(this);
+		screens.add(screen5);
+		
+//		activeScreen = screens.get(0);
+		activeScreen = screens.get(4);
 		
 	}
 	
--- CapstoneProject/src/gameElements/screens/ScreenGame.java
+++ CapstoneProject/src/gameElements/screens/ScreenGame.java
@@ -1,8 +1,6 @@
 package gameElements.screens;
 
 
-
-
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.util.ArrayList;
@@ -16,27 +14,34 @@ public class ScreenGame extends Screen {
 	
 	private DrawingSurface surface;
 	
-	private Rectangle button;
-	private Rectangle multiplayerButton;
+	private Rectangle board;
+	
+	private final int x = 800;
+	private final int y = 600;
 	
 	public ScreenGame(DrawingSurface surface) {
 		super(800,600);
 		this.surface = surface;
 		
-		button = new Rectangle(800/2-100,600/2-50,175,50);
-		multiplayerButton = new Rectangle(800/2-100,600/2+50,175,50);
+		board = new Rectangle(x/2+0,y/2+50,500,400);
 	}
 	
 	
 	public void draw() {
 		
+		surface.rectMode(surface.CENTER);
+		
+		// MOUSE STUFF FOR HELPING DRAW
+		System.out.println(surface.mouseX + "       " + surface.mouseY);
+		
+		
+		// DRAW STUFF
 		surface.pushStyle();
 		surface.background(255,255,255);
 		
 		surface.textSize(20);
 		
-		showButton(button, "Test Database");
-		showButton(multiplayerButton, "Play online");
+		showBox(board);
 		
 		surface.popStyle();
 		
@@ -46,13 +51,13 @@ public class ScreenGame extends Screen {
 	
 	
 	public void mousePressed() {
-		Point p = surface.actualCoordinatesToAssumed(new Point(surface.mouseX,surface.mouseY));
-		
-		if (button.contains(p))
-			surface.switchScreen(ScreenSwitcher.SCREEN2);
-		
-		if (multiplayerButton.contains(p))
-			surface.switchScreen(ScreenSwitcher.SCREEN3);
+//		Point p = surface.actualCoordinatesToAssumed(new Point(surface.mouseX,surface.mouseY));
+//		
+//		if (button.contains(p))
+//			surface.switchScreen(ScreenSwitcher.SCREEN2);
+//		
+//		if (multiplayerButton.contains(p))
+//			surface.switchScreen(ScreenSwitcher.SCREEN3);
 	}
 	
 	private void showButton(Rectangle rectangle, String buttonText) {
@@ -63,6 +68,11 @@ public class ScreenGame extends Screen {
 		surface.text(buttonText, rectangle.x+rectangle.width/2, rectangle.y+rectangle.height/2+6);
 	}
 	
+	private void showBox(Rectangle rectangle) {
+		surface.fill(255);
+		surface.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height, 4);		// 4 is how round the edges is
+		surface.fill(0);
+	}
 
 }
 
