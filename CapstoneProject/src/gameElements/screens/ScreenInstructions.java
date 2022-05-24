69a98d5b9e58b1185c841ad8b790398ce3ed64f3
Nishil Anand <nanand755@student.fuhsd.org>
2022-05-24 16:50:17 -0700

    ScreenInstructions javadocs

--- CapstoneProject/src/gameElements/screens/ScreenInstructions.java
+++ CapstoneProject/src/gameElements/screens/ScreenInstructions.java
@@ -1,29 +1,39 @@
 package gameElements.screens;
 
-
-
-
 import java.awt.Point;
 import java.awt.Rectangle;
-
 import core.DrawingSurface;
 import processing.core.PConstants;
 import core.ImageCodes;
 
 
+/**
+ * This Screen displays basic instructions and controls for playing the game.
+ * 
+ * @author Nishil Anand
+ */
 public class ScreenInstructions extends Screen {
 	
 	private DrawingSurface surface;
+	private Rectangle backButton;				// go back to menu
 	
-	private Rectangle button1;				// go back to menu
 	
+	/**
+	 * Constructs a new ScreenInstructions using a DrawingSurface
+	 * 
+	 * @param surface the DrawingSurface to draw with
+	 */
 	public ScreenInstructions(DrawingSurface surface) {
 		super(1200,600);
 		this.surface = surface;
 		
-		button1 = new Rectangle(1200-200,30,175,50);
+		backButton = new Rectangle(1200-200,30,175,50);
 	}
 	
+	
+	/**
+	 * Draws the elements of the instructions onto the Processing window.
+	 */
 	public void draw() {
 		
 		surface.pushStyle();
@@ -33,10 +43,10 @@ public class ScreenInstructions extends Screen {
 		surface.image(surface.getImages().get(ImageCodes.BACKGROUND), 1200/2, 600/2, 1200, 600);
 		
 		surface.textSize(20);
-		showButton(button1, "Back");
+		showButton(backButton, "Back");
 		
 		surface.tint(255, (float)(255*(3.0/5.0)));
-		surface.image(surface.getImages().get(ImageCodes.BLACK_SQUARE), 1200/2, 600/2+100, 1200-(200*2), 600-(150*2));
+		surface.image(surface.getImages().get(ImageCodes.BLACK_SQUARE), 1200/2, 600/2+100, 1200-(200*2), 600-(140*2));
 		
 		surface.fill(255);
 		
@@ -48,20 +58,24 @@ public class ScreenInstructions extends Screen {
 				 + "\nthere is a king. Your objective is to take the enemy's king before they"
 				 + "\ntake yours! To do this, you have several pieces at your disposal. On "
 				 + "\nthe sides of the board, you will see what pieces are available to you."
-				 + "\nTo add them to the board, press the corresponding number and click to "
-				 + "\nadd it to the board. Note that pieces take up power, which you can see "
-				 + "\non the side. The pieces are arranged in a list, which displays total "
-				 + "\nhealth, damage capacity, and energy needed to use it. Good luck general!", 1200/2, 600/2);
+				 + "\nTo add them to the board, right click on the piece and left click where"
+				 + "\nyou wish to add it to the board. Note that pieces take up power, which"
+				 + "\nyou can see on the side. The pieces are arranged in a list, which displays"
+				 + "\ntotal health, damage capacity, and energy needed to use it. Good luck"
+				 + "\ngeneral!", 1200/2, 550/2);
 		
 		surface.popStyle();
 		
 	}
 	
-	
+	/**
+	 * Called when a mouse is pressed. 
+	 * If the back button is clicked, changes the screen to the menu.
+	 */
 	public void mousePressed() {
 		Point p = surface.actualCoordinatesToAssumed(new Point(surface.mouseX,surface.mouseY));
 		
-		if (button1.contains(p))
+		if (backButton.contains(p))
 			surface.switchScreen(ScreenSwitcher.SCREEN1);
 	}
 	
@@ -73,6 +87,4 @@ public class ScreenInstructions extends Screen {
 		surface.text(buttonText, rectangle.x+rectangle.width/2, rectangle.y+rectangle.height/2+6);
 	}
 	
-
 }
-
