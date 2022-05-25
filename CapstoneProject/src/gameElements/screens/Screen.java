21c1f231bd3ebd5c4ae52c90768d704780305929
Pranav Gunhal <pgunhal761@student.fuhsd.org>
2022-05-24 17:56:18 -0700

    FINAL javadocs + jar

--- CapstoneProject/src/gameElements/screens/Screen.java
+++ CapstoneProject/src/gameElements/screens/Screen.java
@@ -11,25 +11,45 @@ package gameElements.screens;
  */
 public abstract class Screen {
 	
+	/**
+	 * Represents the width and height that this Screen is to be drawn with
+	 * */
 	public final int DRAWING_WIDTH, DRAWING_HEIGHT;
 	
+	/**
+	 * Constructs a new Screen with the specified width and height
+	 * @param width The width of this Screen
+	 * @param height The height of this Screen
+	 * */
 	public Screen(int width, int height) {
 		this.DRAWING_WIDTH = width;
 		this.DRAWING_HEIGHT = height;
 	}
 	
+	/**
+	 * Sets up this Screen by performing actions that only need to be done once
+	 * */
 	public void setup() {
 		
 	}
 	
+	/**
+	 * Draws this Screen
+	 * */
 	public void draw() {
 		
 	}
 	
+	/**
+	 * Performs an action when the mouse is pressed
+	 * */
 	public void mousePressed() {
 		
 	}
 	
+	/**
+	 * Performs an action when a key is pressed
+	 * */
 	public void keyPressed() {
 		
 	}
