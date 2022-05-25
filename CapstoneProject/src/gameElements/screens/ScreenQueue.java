2a45c9dab903a842e4380001813fa3ecf0b6c729
Nishil Anand <nanand755@student.fuhsd.org>
2022-05-24 17:20:44 -0700

    ScreenQueue javadocs

--- CapstoneProject/src/gameElements/screens/ScreenQueue.java
+++ CapstoneProject/src/gameElements/screens/ScreenQueue.java
@@ -1,26 +1,22 @@
 package gameElements.screens;
 
-
-import java.awt.Point;
 import java.awt.Rectangle;
 import java.util.ArrayList;
-import java.util.concurrent.ConcurrentLinkedQueue;
-
-import javax.swing.SwingUtilities;
-
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 import core.DrawingSurface;
-import core.DrawingSurface.DatabaseChangeListener;
 import databaseData.BoardPost;
 import databaseData.IntegerPost;
 import databaseData.UserPost;
 import processing.core.PConstants;
 
+
+/**
+ * This Screen is used as a waiting screen while the user is waiting for another player to play the game.
+ * It also sends both players to a game (so note that both the 1st and 2nd player go through this screen, though the second player might not notice it).
+ * 
+ * @author Nishil Anand
+ */
 public class ScreenQueue extends Screen {
 	
 	private DrawingSurface surface;
@@ -31,9 +27,17 @@ public class ScreenQueue extends Screen {
 	private Rectangle button;
 	
 	private boolean firstLoop;
-	private boolean switched;
 	private DatabaseReference roomCreated;
 	
+	
+	/**
+	 * Constructs a new ScreenQueue
+	 * 
+	 * @param surface the DrawingSurface to draw with
+	 * @param ref the DatabaseReference to the main DatabaseFolder, used for creating/accessing a game room and the queue
+	 * @param queue the ArrayList of UserPosts containing UserPosts in the queue
+	 * @param gameScreen the ScreenOnlineGame that this class helps set up (by setting names and if this player is black or white)
+	 */
 	public ScreenQueue(DrawingSurface surface, DatabaseReference ref, ArrayList<UserPost> queue, ScreenOnlineGame gameScreen) {
 		super(1200,600);
 		this.surface = surface;
@@ -42,36 +46,25 @@ public class ScreenQueue extends Screen {
 		this.gameScreen = gameScreen;
 		this.firstLoop = true;
 		
-		// when there are 2 players in the queue, the one that joined first is white
-		
 		button = new Rectangle(1200/2-100,600/2-50,200,50);
-		switched = false;
-		
 	}
 	
 	
+	/**
+	 * Draws the elements needed to enter a name onto the Processing window.
+	 */
 	public void draw() {
 		
 		// CHECK FOR OPPONENTS
-		
 		String str = "";
 		str = Integer.toString(queue.size());
-		
-		if (switched) {
-			System.err.println("switch failed?");
-		}
 		
 		// make a new game
 		if (firstLoop) {
 			firstLoop = false;
 			if (queue.size() > 0) {		// if there is someone already waiting in queue
-//				System.out.println("quyeuedsadas: " + queue.size());
-				
 				BoardPost board = new BoardPost();
-				DatabaseReference boardRef = surface.postData(board);
-//				gameScreen.setNames("a", "b");
-//				gameScreen.setBoardRef(boardRef);
-//				gameScreen.setBoardRef(gameCreated);
+				surface.postData(board);
 				
 				DatabaseReference gameRef = ref.child(Integer.toString(surface.getI()));
 				surface.postData(new IntegerPost(), gameRef);
@@ -82,62 +75,44 @@ public class ScreenQueue extends Screen {
 				gameScreen.setNames(surface.getPlayerName(), surface.getQueue().get(0).getPlayerName());
 				surface.setInQueue(null);
 				surface.switchScreen(ScreenSwitcher.SCREEN8);
-				switched = true;
 				return;
 			}
 		}
 		
-		
 		// connect to an existing game
 		if (roomCreated != null && queue.size() > 0 && queue.size() % 2 == 0) {
-//			gameScreen.setNames("a", "b");
-//			gameScreen.setBoardRef(gameCreated);
-//			gameScreen.setBoardRef(gameCreated);
-			
-//			String whiteName = gameCreated.getWhiteName();
-//			String blackName = gameCreated.getBlackName();
-			
 			// folder for game replicating	
 			DatabaseReference gameRef = roomCreated;
 			surface.addChildEventListener(gameRef);
 			surface.setGameReference(gameRef);
 			gameScreen.setWhite(false);
-//			NamePost name = new NamePost(whiteName, blackName);
-//			surface.postData(name, gameRef);
-//			gameScreen.setNames(whiteName, blackName);
 			
 			gameScreen.setNames(surface.getQueue().get(1).getPlayerName(), surface.getQueue().get(0).getPlayerName());
 			
 			// remove from queue
-			surface.clearData(ref.child("Queue"));				// TODO: clear data when leaving game, keeping updated i value for new players
+			surface.clearData(ref.child("Queue"));
 			surface.setInQueue(null);
 			surface.switchScreen(ScreenSwitcher.SCREEN8);
-			switched = true;
 			return;
 		}
 		
 		
-		
-		
 		// SCREEN STUFF
 		surface.pushStyle();
-		surface.background(255,255,255);
-		
 		surface.textSize(20);
 		
-//		String str;
-//		if (white) {
-//			str = "white";
-//		} else {
-//			str = "black";
-//		}
-//		showButton(button, str);
+		surface.background(255,255,255);
+		
 		showButton(button, str);
 		
 		surface.popStyle();
 		
 	}
 	
+	/**
+	 * Called when a room is created.
+	 * @param post the DataSnapshot of the room
+	 */
 	public void roomCreated(DataSnapshot post) {
 		System.out.println("ROOM CREATED");
 		String ref = post.getRef().toString();
